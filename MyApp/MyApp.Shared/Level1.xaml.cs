5651592ee6ff790dacc309b81dbf581ebb9c2fa0
Heinrich Eisl <heinrich.eisl@live.com>
2018-06-23 03:07:15 +0200

    Reproducable Binding issue case

--- MyApp/MyApp.Shared/Level1.xaml.cs
+++ MyApp/MyApp.Shared/Level1.xaml.cs
@@ -13,20 +13,16 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
 
 namespace MyApp
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class Level1 : UserControl
     {
-        public MainPage()
+        public Level1()
         {
             this.InitializeComponent();
-            // Remove the comment to fix the binding issue.
-            //this.DataContext = new TestMeModel();
+            this.DataContext = new TestMeModel();
         }
     }
 }
---
+++ MyApp/MyApp.Shared/TestMeModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyApp
+{
+    public class TestMeModel : INotifyPropertyChanged
+    {
+        public TestMeModel()
+        {
+            this.TestMe = "zzzzzzzz";
+            //Task.Delay(4000).ContinueWith(t => AsyncUIContext.Current.InvokeAsync(() => this.TestMe = "uuuuuuuu"));
+        }
+
+        private string _TestMe;
+        public string TestMe
+        {
+            get { return this._TestMe; }
+            set
+            {
+                if (this._TestMe != value)
+                {
+                    this._TestMe = value;
+                    this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(TestMe)));
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
